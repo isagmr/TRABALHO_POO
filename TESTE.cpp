7d99fdf56fbcbf2328a7139c69db597438a509df
isagmr <isabelagmr1004@gmail.com>
2026-06-03 20:09:20 -0300

    Atualização das classes sensores

--- TESTE.cpp
+++ TESTE.cpp
@@ -1,8 +1,11 @@
-//TRABALHO DE PROGRAMAÇÃO ORIENTADA A OBJETOS
-//Quando alguém quiser usar o líquido para um processo, e o tanque estiver no limite baixo, mesmo que a bomba de reabastecimento esteja aberta, precisa ter um alarme que dispara avisando ao usuário que ele não pode usar esse tanque até que ele atinja o nível baixo de segurança + nível que ele quer usar no processo
+// TRABALHO DE PROGRAMAÇÃO ORIENTADA A OBJETOS
+// Quando alguém quiser usar o líquido para um processo, e o tanque estiver no limite baixo, mesmo que a bomba de reabastecimento esteja aberta, precisa ter um alarme que dispara avisando ao usuário que ele não pode usar esse tanque até que ele atinja o nível baixo de segurança + nível que ele quer usar no processo
+// Ver as uilizações da biblioteca stdexcept (erros)
+// incluir sensor de temperatura
+
 #include <iostream>
 #include <string>
-#include <random> // Biblioteca para gerar números aleatórios
+#include <random> // Biblioteca para gerar números aleatórios (o Random Walk do código)
 
 using namespace std;
 //ID_DUPLA = 160 ou 25
@@ -14,9 +17,9 @@ class Sensor{
     double valor;
 
     public: 
-    sensor(string t, string u) : tag(t), unidade(u), valor(0.0) {} // As chaves ficam vazias pois não tem nenhuma outra ação para realizar, já foi inicializado as variáveis
+    Sensor(string t, string u) : tag(t), unidade(u), valor(0.0) {} // As chaves ficam vazias pois não tem nenhuma outra ação para realizar, já foi inicializado as variáveis
 
-    virtual ~sensor(){} // Destruidor de objetos/classes - me parece EXTREMAMENTE desnecessário mas mantive para ver a diferença que faz no código
+    virtual ~Sensor(){} // Destruidor de objetos/classes - me parece EXTREMAMENTE desnecessário mas mantive para ver a diferença que faz no código
 
     virtual void simular() = 0; // Cada sensor simula de uma maneira diferente
 
@@ -24,7 +27,7 @@ class Sensor{
     // 'return tag' para retornar o nome de cada sensor
     string getTag() const {return tag;}
     string getUnidade() const {return unidade;}
-    string getValor() const {return valor;}
+    double getValor() const {return valor;}
     };
 
 // SensorNivel herdando Sensor
@@ -32,54 +35,128 @@ class SensorNivel : public Sensor {
     public:
     // Chama o construtor e
     // 'Empurra' o texto "t" para a classe "mãe" e avisa que a unidade de medida utilizada nesta classe será %
-    SensorNivel(string t) : sensor(t, "%") {
+    SensorNivel(string t) : Sensor(t, "%") {
         valor = 70.0;
-} 
+    } 
     // Para a função simular vamos tentar usar o conceito chamado "Caminhada Aleatória (Random Walk)"
     // Em vez de sortear números completamente novos e aleatórios pegamos o 'valor' e sorteamos uma pequana variação (uma "pertubação") para subtrair dele
     void simular() override { 
-        // 1. Configurar o gerador de números aleatórios do C++
+        
+        if(estacao.LigarBomba = 1){ // Se a bomba estiver ligada 
+            static std::random_device rd;
+	        static std::mt19937 gen(rd());
+            // 2. Definir que a variação vai ser pequena: entre [1.5% e 1.5%] 
+	        std::uniform_real_distribution<double> variacao(1.5, 1.5)
+            // 3. Aplicar a pequena variação no valor atual
+	        valor += variacao(gen);
+            // 4. Segurança: não deixa o reservatório passsar do 100% nem ficar negativo (< 0.0%)
+	        if(valor > 100.0){
+		    valor = 100.0;
+	        }
+	        if(valor < 0.0){
+		    valor = 0.0;
+	        }    
+        }
+        if else(estacao.AbrirValvula = 1){
+            static std::random_device rd;
+	        static std::mt19937 gen(rd());
+            // 2. Definir que a variação vai ser pequena: entre [-1.5% e -1.5%] 
+	        std::uniform_real_distribution<double> variacao(-1.5, -1.5)
+            // 3. Aplicar a pequena variação no valor atual
+	        valor += variacao(gen);
+            // 4. Segurança: não deixa o reservatório passsar do 100% nem ficar negativo (< 0.0%)
+	        if(valor > 100.0){
+		    valor = 100.0;
+	        }
+	        if(valor < 0.0){
+		    valor = 0.0;
+	        } 
+        }
+        else{
+            // 1. Configurar o gerador de números aleatórios do C++
+	        static std::random_device rd;
+	        static std::mt19937 gen(rd());
 
-        // 2. Definir que a variação vai ser pequena: entre [-X.X% e X.X%] 
+            // 2. Definir que a variação vai ser pequena: entre [-1.5% e 1.5%] 
+	        std::uniform_real_distribution<double> variacao(-1.5, 1.5)
 
-        // 3. Aplicar a pequena variação no valor atual
+            // 3. Aplicar a pequena variação no valor atual
+	        valor += variacao(gen);
 
-        // 4. Segurança: não deixa o reservatório passsar do 100% nem ficar negativo [(< 0.0%)]
-    }};
+            // 4. Segurança: não deixa o reservatório passsar do 100% nem ficar negativo (< 0.0%)
+	        if(valor > 100.0){
+		        valor = 100.0;
+	        }
+	        if(valor < 0.0){
+		        valor = 0.0;
+	        }
+        }
+    }
+};
 
-    // Essa aqui é a classe (Pai) - é um 'contrato' que as regras devem seguir 
-    // Armazena a estrutura principal de todas as classes que forem expicificamente de alguma regra do sistema
+class SensorPressao : public Sensor {
+    SensorPressao(string t) : Sensor(t, bar){
+        pressao = 0.0;
+    }
+    void simular (){
+        if(estacao.BombaLigada = 1){
+            static std::random_device rd;
+	        static std::mt19937 gen(rd());
+            // 2. Definir que a variação vai ser pequena: entre [1.5% e 1.5%] 
+	        std::uniform_real_distribution<double> variacao(1.5, 1.5)
+            // 3. Aplicar a pequena variação no valor atual
+	        valor += variacao(gen);
+            // 4. Segurança: não deixa o reservatório passsar do 100% nem ficar negativo (< 0.0%)
+	        if(valor > 100.0){
+		    valor = 100.0;
+	        }
+	        if(valor < 0.0){
+		    valor = 0.0;
+	        }    
+        }
+    }
+
+};
+
+// Essa aqui é a classe (Pai) - é um 'contrato' que as regras devem seguir 
+// Armazena a estrutura principal de todas as classes que forem expicificamente de alguma regra do sistema
 class RegraControle{
     public:
     virtual void aplicar(EstacaoBombeamento& estacao) = 0; // '= 0' significa: função sem lógica aqui "{}", as classes filhas são OBRIGADAS a programar a lógica para cada classe especifica
-    };
+};
 
-// Quando o nível da estação estiver abaixo de ['X']
+// Quando o nível da estação estiver abaixo de 27.5
 class NivelBaixo : public RegraControle{
     public:
     void aplicar(EstacaoBombeamento& estacao) override {
         // sem '= 0' aqui, pois agora estamos escrevendo a lógica 
         // 'override' garante que estamos cumprindo o contrato do 'pai'
-        if (estacao.nivel < [27.5]){ // % -> unidade de medida pedido na especificação do projeto
+        if (estacao.nivel < 27.5){ // % -> unidade de medida pedido na especificação do projeto
             estacao.ligarbomba;
-}}};
+        }
+    }
+};
 
-//Quando a pressão da estação estiver maior ou igual a ['X']
+//Quando a pressão da estação estiver maior ou igual a 6.5
 class PressaoAlta : public RegraControle{
     public:
     void aplicar(EstacaoBombeamento& estacao) override{
         // sem '= 0' aqui, pois agora estamos escrevendo a lógica 
         // 'override' garante que estamos cumprindo o contrato do 'pai'
-        if(estacao.pressao >= [6.5]) { // bar -> unidade de medida pedido na especificação do projeto
+        if(estacao.pressao >= 6.5) { // bar -> unidade de medida pedido na especificação do projeto
             estacao.desligarbomba;
-}}};
+        }
+    }
+};
 
-// Quando o nível da estação estiver acima de ['X']
+// Quando o nível da estação estiver acima de 82.5
 class NivelAlto : public RegraControle{
     public:
     void aplicar(EstacaoBombeamento& estacao) override{
         // sem '= 0' aqui, pois agora estamos escrevendo a lógica 
         // 'override' garante que estamos cumprindo o contrato do 'pai'
-        if(estacao.nivelAlto > [82.5]){ // % -> unidade de medida pedido na especificação do projeto
+        if(estacao.nivelAlto > 82.5){ // % -> unidade de medida pedido na especificação do projeto
             estacao.DesligarBomba; // Criar função DesligarBomba
-}}};
+        }
+    }
+};
